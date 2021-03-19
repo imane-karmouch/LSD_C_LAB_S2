d42601a1bb410b2f019c6732735f8b1c02dd3431
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2021-03-19 12:15:07 +0100

    Update recursive_fibo.c

--- TP1/EX1/recursive_fibo.c
+++ TP1/EX1/recursive_fibo.c
@@ -2,5 +2,13 @@
 
 int recursive_fibo(int n)
 {
-    // write your recursive fibo code here
+    if (n==0)
+	{
+		return 0;
+	}
+	if (n==1)
+	{
+		return 1;
+	}
+	return (Fibb_recur(n-1)+Fibb_recur(n-2));
 }
