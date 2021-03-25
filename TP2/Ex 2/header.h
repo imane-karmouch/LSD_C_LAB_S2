26765fe2a60f71e02f58e5ceb730096e34da3831
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2021-03-25 23:09:45 +0100

    Update header.h

--- TP2/Ex 2/header.h
+++ TP2/Ex 2/header.h
@@ -13,9 +13,9 @@ typedef struct node node;
 typedef node* t_list;
 
 t_list create_node(int element);
-void add_node(int value, t_list L);
+void add_node(int element, t_list L);
 t_list array_to_list(int element, int T[]);
-void print list(t_list L);
+void print_list(t_list L);
 void remove_node(t_list *L,int i);
 
 #endif
