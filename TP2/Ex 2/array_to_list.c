55ab63c0b883d5749db5bd6e7d9e30cfc467dc91
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2021-03-26 10:31:48 +0100

    Update array_to_list.c

--- TP2/Ex 2/array_to_list.c
+++ TP2/Ex 2/array_to_list.c
@@ -1,5 +1,10 @@
 #include"header.h"
-t_list array_to_list(int data, int T[])
+t_list array_to_list(int n, int T[])
 {
-  
+  t_list L;
+  L=create_node(T[0]);
+  for (i=0;i<n;i++){
+    add_node(L,T[i]);
+  }
+  return L;
 }
