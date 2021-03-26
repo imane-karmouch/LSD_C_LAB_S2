39897e3367fb9379c650cbcbeeae9f88638ff840
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2021-03-26 10:58:31 +0100

    Update main.c

--- TP2/Ex 2/main.c
+++ TP2/Ex 2/main.c
@@ -1,1 +1,11 @@
-
+#include"header.h"
+int main(){
+  int data;
+  printf("entrer le nombre du premier element de la liste: \n");
+	scanf("%d", &data);
+  t_list L=create_node(data);
+  print_list(L);
+  
+  
+  
+}
