87400649116a7fa9b766db5b0dab5f8285679903
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2021-03-26 10:13:42 +0100

    Update add_node.c

--- TP2/Ex 2/add_node.c
+++ TP2/Ex 2/add_node.c
@@ -1,12 +1,15 @@
 #include "header.h"
 
-t_list add_node(int data, t_list L)
+void add_node(int data, t_list L)
 {
-  t_list C=create_node(data);
-  if (L==NULL){
-    L=C;
+ t_list C=create_node(data);
+ if(*L==NULL)
+     *L=C;
+ else {
+    t_list temp=*L;
+    while(temp->next!=NULL)
+        temp=temp->next;
+    temp->suivant=C;
   }
-  else{
-    while 
-  
+  return;
 }
