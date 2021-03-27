19b3c10a0bf2091d10c0d4325e17c0dc9f6471b8
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2021-03-27 12:23:38 +0100

    Update header.h

--- TP2/Ex1/header.h
+++ TP2/Ex1/header.h
@@ -14,7 +14,6 @@ typedef struct s_point t_point;
 char  **allocated_array(t_point T);
 void array_inputs(char **d,t_point T);
 void array_output(char **d,t_point T);
-
-
+void flood_fill(char** d, t_point T,t_point dim,char s, char p);
 
 #endif
