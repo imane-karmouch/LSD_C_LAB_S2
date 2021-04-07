92aafc4c353be3748fa62bc5e9268d38a1bc48f3
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2021-04-07 22:17:36 +0200

    Update palindrome.c

--- TP3/Ex1/palindrome.c
+++ TP3/Ex1/palindrome.c
@@ -1,11 +1,25 @@
 #include "header.h"
 int palindrome(listeDouble L)
 {
-	n=longueur(L);
-	listeDouble temp=L;
-	while (i<n)
+	//palindrome=1
+	//n'est pas palindrome=0
+	unsigned n;
+	int i;
+	n=L.longueur;
+	celluleDouble *L1=L.debut;
+	celluleDouble *L2=L.fin;
+	if(listeDoubleVide(L)==1)
+		return 0;
+	for (i=0;i<n%2;i++)
 	{
-		if (temp->
-	}
-	
+
+		if (L1->element!=L2->element)
+			return 0;
+		else
+			{
+				L1=L1->suivant;
+				L2=L2->precedent;
+			}
+	} 
+	return 1;
 }
