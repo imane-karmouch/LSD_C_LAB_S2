8b71e991ab05f5de5161db54fa34dda7fa66fe9f
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2021-04-07 22:18:11 +0200

    Update main.c

--- TP3/Ex1/main.c
+++ TP3/Ex1/main.c
@@ -1,1 +1,18 @@
 
+int main (int ac,char **av)
+{
+	listeDouble L={NULL,NULL,0};
+	int T;
+	printf("%d\n",ac);
+	printf("%s\n",av[1]);
+	L=convert_string(av[1]);
+	T=palindrome(L);
+	printf("%d\n",T);
+	int i=0;
+	while(i<ac)
+	{
+		printf("%s\n",av[i]);
+		i++;
+	}
+	return 0;
+}
