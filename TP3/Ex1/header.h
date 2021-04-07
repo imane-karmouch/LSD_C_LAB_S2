50c9c9071b960c6604fd44fd249abd22952cde8e
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2021-04-07 22:20:22 +0200

    Update header.h

--- TP3/Ex1/header.h
+++ TP3/Ex1/header.h
@@ -19,6 +19,30 @@ struct listeDouble
     unsigned longueur;
 };
 typedef struct listeDouble listeDouble;
+int listeDoubleVide(listeDouble L)
+{
+    if(L.debut==NULL)
+        return 1;
+    return 0;
+}
+void ajoutDebut(int x,listeDouble * L)
+{
+    celluleDouble * C=(celluleDouble *)malloc(sizeof(celluleDouble));
+    C->element=x;
+    C->suivant=L->debut;
+    C->precedent=NULL;
+    if(listeDoubleVide(*L)==1)
+    {
+        L->debut=C;
+        L->fin=C;
+        L->longueur=1;
+        return;
+    }
+    L->debut->precedent=C;
+    L->debut=C;
+    L->longueur+=1;
+    return;
+}
 void ajoutFin(char x,listeDouble *L)
 {
     celluleDouble * C=(celluleDouble *)malloc(sizeof(celluleDouble));
