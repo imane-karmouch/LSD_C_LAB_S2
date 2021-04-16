048fec60189debe7e39444d908329383b95bcf67
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2021-04-16 13:27:01 +0200

    Update main.c

--- TP3/Ex 2/main.c
+++ TP3/Ex 2/main.c
@@ -1,1 +1,34 @@
+#include "header.h"
+int main() 
+{
+    time_t t;
+    int i;
+    int j;
+    list L;
+    list T;  
+    list M;
+    list S;
+    L = T = M = S= NULL;
+    srand((unsigned) time(&t));
 
+    for (i=0; i<5; i++){
+      ajoutFin(rand()%100+1,&L);
+    }
+    for (j=0; j<4; j++){
+      ajoutFin(rand()%100+1,&T);
+    }
+    affichageListe(L);
+    affichageListe(T);
+
+    L=sort_list(L);
+    T=sort_list(T);
+    affichageListe(L);
+    affichageListe(T);
+
+    M=merge_liste(L,T);
+    affichageListe(M);
+    S=remove_duplicated(M);
+    affichageListe(S);
+
+    return 0;
+}
