774a8588c1b070a6e6996fe3ce08039a66e25e1e
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2021-05-10 01:32:55 +0200

    Update main.c

--- TP3/Ex3/main.c
+++ TP3/Ex3/main.c
@@ -1,1 +1,16 @@
-
+#include "header.h"
+int main(){
+    int l;
+    int s;
+    t_stack *P=new_stack(5);
+    push(&P,7);
+    push(&P,2);
+    print_stack(P);
+    l=stack_len(P);
+    printf("%d\n",l);
+    s=peek_stack(P);
+    printf("%d\n",s);
+    pop(&P);
+    print_stack(P);
+    return 0;
+}
