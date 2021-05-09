4f5d1c14e195dda7994941a8209df017d7913f48
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2021-05-10 01:36:44 +0200

    Update header.h

--- TP3/Ex4/header.h
+++ TP3/Ex4/header.h
@@ -1,1 +1,11 @@
+#ifndef HEADER
+#define HEADER
 
+# include <stdlib.h>
+# include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+# include <limits.h> 
+
+#include "header.h" //the header related to ex 3
+#endif
