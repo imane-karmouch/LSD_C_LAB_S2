3d53f9f7c0de6f5b42ac2393e64338e49e0398fc
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2021-06-10 15:51:09 +0200

    Create max_depth.c

---
+++ TP4/ex2/max_depth.c
@@ -0,0 +1,11 @@
+#include "header.h"
+int max_depth(tree T){
+    if (T==NULL)
+        return 0;
+    if(T->left==NULL && T->right==NULL)
+        return 1;
+    if (T->left==NULL)
+        return 
+
+
+}
