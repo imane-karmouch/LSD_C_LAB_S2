f4a0c6553384b5acab420c146cc0358e44375798
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2021-06-10 15:52:58 +0200

    Update tree_size.c

--- TP4/ex2/tree_size.c
+++ TP4/ex2/tree_size.c
@@ -1,1 +1,12 @@
-
+#include "header.h"
+int tree_size(tree T){
+    if (T==NULL)
+        return 0;
+    if (T->left==NULL && T->right==NULL)
+        return 1;
+    if (T->left==NULL)
+        return 1+tree_size(T->right);
+    if (T->right==NULL)
+        return 1+tree_size(T->left);
+    return 1+tree_size(T->left)+tree_size(T->right);
+}
